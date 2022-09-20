91252862647b566aee72bc4fe87d217af6cac1a6
Roman Babanov <babanov.js@gmail.com>
2022-09-20 20:27:40 +0300

    design fixes

--- src/components/UsersGroup/UsersGroup.jsx
+++ src/components/UsersGroup/UsersGroup.jsx
@@ -2,19 +2,22 @@ import styles from './UsersGroup.module.scss';
 import React from 'react';
 import { UserAvatar } from '../UserAvatar/UserAvatar';
 
-export const UsersGroup = ({ users, transformProps }) => {
+export const UsersGroup = ({ title, users, transformProps }) => {
   return (
-    <div className={styles.group}>
-      {users &&
-        users.map((user) => {
-          return (
-            <UserAvatar
-              key={user.id}
-              user={user}
-              transformProps={transformProps}
-            />
-          );
-        })}
+    <div className={styles.groupWrapper}>
+      <h4>{title}</h4>
+      <div className={styles.group}>
+        {users &&
+          users.map((user) => {
+            return (
+              <UserAvatar
+                key={user.id}
+                user={user}
+                transformProps={transformProps}
+              />
+            );
+          })}
+      </div>
     </div>
   );
 };
--- src/constants/OrgStructure.js
+++ src/constants/OrgStructure.js
@@ -15,8 +15,8 @@ export const MOCK_DATA = {
       },
       {
         id: 3,
-        userName: 'Michael Yang',
-        pic: 'https://scontent.xx.fbcdn.net/v/t31.18172-8/18056228_1293524460682598_7016211040260639160_o.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Ygx1_Flney8AX-p_Pst&tn=AtZ_D2jduJx6PXv3&_nc_ht=scontent.xx&oh=00_AT9sJFYrgjVfAerXCJaSBgDcXTi_zECJhDMfMMyDHIqKNQ&oe=6350D96D',
+        userName: 'John Week',
+        pic: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyc3xlbnwwfHwwfHw%3D&w=1000&q=80',
       },
     ],
   },
@@ -26,35 +26,103 @@ export const MOCK_DATA = {
       {
         id: '1',
         title: 'Inbox',
+        users: [
+          {
+            id: 1,
+            userName: 'Ivan Inav',
+            pic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1ZrY2oBOhXShBBonWAzjKK9SgMASWsmsptYmJiU9Xr6tsNmxjZXZOp0wKZuesIM3jZZk&usqp=CAU',
+          },
+          {
+            id: 2,
+            userName: 'Dmitry Dmitry',
+            pic: 'https://i0.wp.com/roohentertainment.com/wp-content/uploads/2018/06/user-avatar-1.png?ssl=1',
+          },
+          {
+            id: 3,
+            userName: 'John Week',
+            pic: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyc3xlbnwwfHwwfHw%3D&w=1000&q=80',
+          },
+        ],
       },
       {
         id: '2',
         title: 'Inbox',
+        users: [
+          {
+            id: 1,
+            userName: 'Ivan Inav',
+            pic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1ZrY2oBOhXShBBonWAzjKK9SgMASWsmsptYmJiU9Xr6tsNmxjZXZOp0wKZuesIM3jZZk&usqp=CAU',
+          },
+          {
+            id: 2,
+            userName: 'Dmitry Dmitry',
+            pic: 'https://i0.wp.com/roohentertainment.com/wp-content/uploads/2018/06/user-avatar-1.png?ssl=1',
+          },
+          {
+            id: 3,
+            userName: 'John Week',
+            pic: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyc3xlbnwwfHwwfHw%3D&w=1000&q=80',
+          },
+        ],
       },
       {
         id: '3',
         title: 'Inbox',
+        users: [
+          {
+            id: 1,
+            userName: 'Ivan Inav',
+            pic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1ZrY2oBOhXShBBonWAzjKK9SgMASWsmsptYmJiU9Xr6tsNmxjZXZOp0wKZuesIM3jZZk&usqp=CAU',
+          },
+          {
+            id: 2,
+            userName: 'Dmitry Dmitry',
+            pic: 'https://i0.wp.com/roohentertainment.com/wp-content/uploads/2018/06/user-avatar-1.png?ssl=1',
+          },
+          {
+            id: 3,
+            userName: 'John Week',
+            pic: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyc3xlbnwwfHwwfHw%3D&w=1000&q=80',
+          },
+        ],
       },
       {
         id: '4',
         title: 'Inbox',
+        users: [
+          {
+            id: 1,
+            userName: 'Ivan Inav',
+            pic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1ZrY2oBOhXShBBonWAzjKK9SgMASWsmsptYmJiU9Xr6tsNmxjZXZOp0wKZuesIM3jZZk&usqp=CAU',
+          },
+          {
+            id: 2,
+            userName: 'Dmitry Dmitry',
+            pic: 'https://i0.wp.com/roohentertainment.com/wp-content/uploads/2018/06/user-avatar-1.png?ssl=1',
+          },
+          {
+            id: 3,
+            userName: 'John Week',
+            pic: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyc3xlbnwwfHwwfHw%3D&w=1000&q=80',
+          },
+        ],
       },
     ],
     users: [
       {
         id: 1,
-        userName: 'Anastasia Shchogoleva',
-        pic: 'https://scontent.xx.fbcdn.net/v/t1.6435-9/37059064_866439313554885_4490165901676511232_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=h_KwFdBCngYAX_k0NvI&_nc_ht=scontent.xx&oh=00_AT_SDd8UdtVhfZef1oMTVr3KMl834rjxQTQtUGYRggcAeQ&oe=63512B95',
+        userName: 'Ivan Inav',
+        pic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1ZrY2oBOhXShBBonWAzjKK9SgMASWsmsptYmJiU9Xr6tsNmxjZXZOp0wKZuesIM3jZZk&usqp=CAU',
       },
       {
         id: 2,
-        userName: 'Dmitry Kushnikov',
-        pic: 'https://scontent.xx.fbcdn.net/v/t39.30808-6/275296336_10159696246244730_2290573779909155628_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=gw2VaqOpCZAAX8Q8zlg&tn=AtZ_D2jduJx6PXv3&_nc_ht=scontent.xx&oh=00_AT-bjjrLciNnQk2Iu6iv1f9-ECZxThHjtC66wzR5so-Ufg&oe=632DECE7',
+        userName: 'Dmitry Dmitry',
+        pic: 'https://i0.wp.com/roohentertainment.com/wp-content/uploads/2018/06/user-avatar-1.png?ssl=1',
       },
       {
         id: 3,
-        userName: 'Michael Yang',
-        pic: 'https://scontent.xx.fbcdn.net/v/t31.18172-8/18056228_1293524460682598_7016211040260639160_o.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Ygx1_Flney8AX-p_Pst&tn=AtZ_D2jduJx6PXv3&_nc_ht=scontent.xx&oh=00_AT9sJFYrgjVfAerXCJaSBgDcXTi_zECJhDMfMMyDHIqKNQ&oe=6350D96D',
+        userName: 'John Week',
+        pic: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyc3xlbnwwfHwwfHw%3D&w=1000&q=80',
       },
     ],
   },
