02ba9fba5a857afedfcabb6671746d14c282730c
Roman Babanov <babanov.js@gmail.com>
2022-10-10 13:32:41 +0300

    fix transform params

--- src/constants/TransformParams.js
+++ src/constants/TransformParams.js
@@ -1,7 +1,7 @@
-export const MAX_SCALE = 3;
-export const MIDDLE_SCALE = 1.5;
-export const LARGE_SCALE = 2;
 export const MIN_SCALE = 0.5;
+export const MIDDLE_SCALE = 1;
+export const LARGE_SCALE = 1.5;
+export const MAX_SCALE = 2;
 export const ZOOM_STEP = 0.05;
 export const DOUBLE_CLICK_STEP = 1;
 export const INITIAL_SCALE = 0.5;
