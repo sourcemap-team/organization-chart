489ff7b70cb5946b0d95697e1db1f5865a02e992
Roman Babanov <babanov.js@gmail.com>
2022-10-26 13:42:12 +0400

    add creds

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,8 @@ const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL || '/',
 });
 
+instance.defaults.withCredentials = true;
+
 export const fetchOrgChartData = async () => {
   try {
     const { data } = await instance.get('/orgchart/data', {
